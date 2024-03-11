2beecf7a762725c43efc618d4d22df3357a046b0
jorgeVerduzco <158482565+jorgeVerduzco@users.noreply.github.com>
2024-03-11 13:10:01 -0400

    Add files via upload

---
+++ DraggedShips.java
@@ -0,0 +1,14 @@
+package GameTest;
+import java.awt.Point;
+import javax.swing.ImageIcon;
+public class DraggedShips {
+    Point imageUpperLeft, prevPoint;
+    ImageIcon imageHorazontal, imageVertical;
+    int length;
+    boolean isHorizontal;
+
+    public DraggedShips(String Path, String verticalPath, Point start, int len)
+    {
+        
+    }
+}
---
+++ Ship.java
@@ -0,0 +1,59 @@
+package GameTest;
+
+public class Ship {
+    private String name;
+    private int size;
+    private int[] startCoordinates;
+    private boolean isHorizontal;
+    private boolean[] hitMarkers;
+
+    public Ship(String name, int size,int row, int col, boolean isHorizontal) {
+        this.name = name;
+        this.size = size;
+        this.startCoordinates = new int[] {row, col};
+        this.isHorizontal = isHorizontal;
+        this.hitMarkers = new boolean[size];
+    }
+
+    public boolean markHit(int row, int col) {
+        //calculate hit index based on ship orientation and starting position
+       int hitIndex = isHorizontal ? col  - startCoordinates[1] : row - startCoordinates[0];
+       if(hitIndex >= 0 && hitIndex < size) {
+        if(!hitMarkers[hitIndex]) {
+            hitMarkers[hitIndex] = true;
+            return true; //hit successful
+        }
+       }
+       return false; //hit not successful
+    }
+    public boolean isSunk() {
+        for(boolean hit : hitMarkers) {
+            if(!hit) {
+                return false; //if parts not hit ship isnt sunk
+            }
+        }
+        return true; //all parts hit
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int[] getStartCoordinates() {
+        return startCoordinates;
+    }
+
+    public boolean isHorizontal() {
+        return isHorizontal;
+    }
+
+    
+}
